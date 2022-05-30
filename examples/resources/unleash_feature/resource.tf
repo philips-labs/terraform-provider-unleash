5df9e645838385b5f95620c10aa843eb54f5ad46
Joana Deluca Kleis <13077810+jdelucaa@users.noreply.github.com>
2022-05-30 17:20:15 -0300

    Add support for feature variants (#25)
    * Add support for feature variants
    
    * set weight as computed

--- examples/resources/unleash_feature/resource.tf
+++ examples/resources/unleash_feature/resource.tf
@@ -11,3 +11,27 @@ resource "unleash_feature" "example" {
   project_id = data.unleash_project.example.project_id
   type       = data.unleash_feature_type.example.type_id
 }
+
+resource "unleash_feature" "variants_example" {
+  name       = "toggle_variants"
+  project_id = data.unleash_project.example.project_id
+  type       = data.unleash_feature_type.example.type_id
+  variant {
+    name = "Variant1"
+  }
+  variant {
+    name = "Variant2"
+    payload {
+      type  = "string"
+      value = "foo"
+    }
+    overrides {
+      context_name = "appName"
+      values       = ["bar", "xyz"]
+    }
+    overrides {
+      context_name = "environment"
+      values       = ["development"]
+    }
+  }
+}
--- internal/provider/resource_feature_test.go
+++ internal/provider/resource_feature_test.go
@@ -20,6 +20,14 @@ func TestAccResourceFeature(t *testing.T) {
 					resource.TestMatchResourceAttr("unleash_feature.foo", "name", regexp.MustCompile("^bar")),
 					resource.TestMatchResourceAttr("unleash_feature.foo", "project_id", regexp.MustCompile("^default$")),
 					resource.TestMatchResourceAttr("unleash_feature.foo", "type", regexp.MustCompile("^release$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.name", regexp.MustCompile("^Variant$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.payload.0.type", regexp.MustCompile("^string$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.payload.0.value", regexp.MustCompile("^foo$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.overrides.0.context_name", regexp.MustCompile("^appName$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.overrides.0.values.0", regexp.MustCompile("^bar$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.overrides.0.values.1", regexp.MustCompile("^xyz$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.overrides.1.context_name", regexp.MustCompile("^environment$")),
+					resource.TestMatchResourceAttr("unleash_feature.foo", "variant.0.overrides.1.values.0", regexp.MustCompile("^development$")),
 				),
 			},
 		},
@@ -31,5 +39,20 @@ resource "unleash_feature" "foo" {
   name = "bar%s"
   project_id = "default"
   type = "release"
+  variant {
+	name = "Variant"
+	payload {
+	  type  = "string"
+	  value = "foo"
+	}
+	overrides {
+	  context_name = "appName"
+	  values       = ["bar", "xyz"]
+	}
+	overrides {
+	  context_name = "environment"
+	  values       = ["development"]
+	}
+  }
 }
 `, utils.RandomString(4))
