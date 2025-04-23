44e19ed045538b41106dba4a7e263a1caea6ca6b
Joana Deluca Kleis <joana.deluca@philips.com>
2025-04-23 10:43:29 -0300

    test resource updates

--- internal/provider/resource_api_token_test.go
+++ internal/provider/resource_api_token_test.go
@@ -10,26 +10,56 @@ import (
 )
 
 func TestAccResourceApiToken(t *testing.T) {
+	randomSuffix := utils.RandomString(4)
+
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceApiToken,
+				// Initial creation
+				Config: testAccResourceApiTokenInitial(randomSuffix),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestMatchResourceAttr("unleash_api_token.foo", "username", regexp.MustCompile("^bar")),
 					resource.TestMatchResourceAttr("unleash_api_token.foo", "secret", regexp.MustCompile(`^\*:development\.`)),
+					resource.TestCheckResourceAttr("unleash_api_token.foo", "environment", "development"),
+					resource.TestCheckResourceAttr("unleash_api_token.foo", "projects.#", "1"),
+				),
+			},
+			{
+				// Update configuration
+				Config: testAccResourceApiTokenUpdated(randomSuffix),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify updates took effect
+					resource.TestMatchResourceAttr("unleash_api_token.foo", "expires_at", regexp.MustCompile("^2023-04-15T14:30:45Z$")),
+					// Verify unchanged attributes
+					resource.TestMatchResourceAttr("unleash_api_token.foo", "username", regexp.MustCompile("^bar")),
+					resource.TestMatchResourceAttr("unleash_api_token.foo", "secret", regexp.MustCompile(`^\*:development\.`)),
+					resource.TestCheckResourceAttr("unleash_api_token.foo", "environment", "development"),
+					resource.TestCheckResourceAttr("unleash_api_token.foo", "projects.#", "1"),
 				),
 			},
 		},
 	})
 }
 
-var testAccResourceApiToken = fmt.Sprintf(`
+func testAccResourceApiTokenInitial(suffix string) string {
+	return fmt.Sprintf(`
 resource "unleash_api_token" "foo" {
   username    = "bar%s"
   type        = "client"
   environment = "development"
   projects    = ["*"]
+}`, suffix)
 }
-`, utils.RandomString(4))
+
+func testAccResourceApiTokenUpdated(suffix string) string {
+	return fmt.Sprintf(`
+resource "unleash_api_token" "foo" {
+  username    = "bar%s"
+  type        = "client"
+  environment = "development"
+  projects    = ["*"]
+	expires_at = "2023-04-15T14:30:45Z"
+}`, suffix)
+}
--- internal/provider/resource_user_test.go
+++ internal/provider/resource_user_test.go
@@ -10,26 +10,50 @@ import (
 )
 
 func TestAccResourceUser(t *testing.T) {
+	randomSuffix := utils.RandomString(4)
+
 	resource.UnitTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccResourceUser,
+				// Initial creation
+				Config: testAccResourceUserInitial(randomSuffix),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestMatchResourceAttr("unleash_user.foo", "name", regexp.MustCompile("^bar")),
 					resource.TestMatchResourceAttr("unleash_user.foo", "email", regexp.MustCompile("^foo.+@foo.com.br$")),
 					resource.TestMatchResourceAttr("unleash_user.foo", "root_role", regexp.MustCompile("^Admin$")),
 				),
 			},
+			{
+				// Update configuration
+				Config: testAccResourceUserUpdated(randomSuffix),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					// Verify updates took effect
+					resource.TestMatchResourceAttr("unleash_user.foo", "root_role", regexp.MustCompile("^Viewer$")),
+					// Verify unchanged attributes
+					resource.TestMatchResourceAttr("unleash_user.foo", "name", regexp.MustCompile("^bar")),
+					resource.TestMatchResourceAttr("unleash_user.foo", "email", regexp.MustCompile("^foo.+@foo.com.br$")),
+				),
+			},
 		},
 	})
 }
 
-var testAccResourceUser = fmt.Sprintf(`
+func testAccResourceUserInitial(suffix string) string {
+	return fmt.Sprintf(`
 resource "unleash_user" "foo" {
   name = "bar"
   email = "foo%s@foo.com.br"
   root_role = "Admin"
+}`, suffix)
 }
-`, utils.RandomString(4))
+
+func testAccResourceUserUpdated(suffix string) string {
+	return fmt.Sprintf(`
+resource "unleash_user" "foo" {
+  name = "bar"
+  email = "foo%s@foo.com.br"
+  root_role = "Viewer"
+}`, suffix)
+}
